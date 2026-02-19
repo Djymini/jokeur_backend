eb3e14205983394bf0a9478de9b459778f55cfd9
yol97 <yol974@hotmail.com>
2026-02-19 11:22:31 +0100

    test after changes

--- src/main/java/com/ashleydev/jokeur_api/persistence/entities/UserEntity.java
+++ src/main/java/com/ashleydev/jokeur_api/persistence/entities/UserEntity.java
@@ -2,9 +2,7 @@ package com.ashleydev.jokeur_api.persistence.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -13,15 +11,11 @@ import java.util.Collections;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
+/* @Getter
+@Setter */
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserEntity extends BaseEntity implements UserDetails {
-
-    /* @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; */
 
     @Column(nullable = false, unique = true)
     private String pseudo;
@@ -44,7 +38,7 @@ public class UserEntity extends BaseEntity implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    /* 🔐 Méthodes UserDetails (utilisées par Spring Security) */
+    /* Méthodes UserDetails (utilisées par Spring Security) */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Collections.singletonList(() -> "ROLE_" + role.name());
@@ -71,4 +65,56 @@ public class UserEntity extends BaseEntity implements UserDetails {
 
     @Override
     public boolean isEnabled() { return true; }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
