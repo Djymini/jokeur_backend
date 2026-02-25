8d6d1c1f64c9792a0486b2487bccf49f941debed
Ashley <laboryashley@gmail.com>
2026-02-25 15:29:20 +0100

    pettier changes

--- src/main/java/com/ashleydev/jokeur_api/exceptions/storage/FileStorageException.java
+++ src/main/java/com/ashleydev/jokeur_api/exceptions/storage/FileStorageException.java
@@ -2,7 +2,7 @@ package com.ashleydev.jokeur_api.exceptions.storage;
 
 public class FileStorageException extends RuntimeException {
 
-    public FileStorageException(String message, Throwable cause) {
-        super(message, cause);
-    }
+  public FileStorageException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
