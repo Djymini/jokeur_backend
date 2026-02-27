0e356f2038c1b9a4d2e35927c7b5b1f7c97721f6
yol97 <yol974@hotmail.com>
2026-02-27 10:01:10 +0100

    save before PR with scheduler changes

--- src/main/java/com/ashleydev/jokeur_api/scheduler/RssScheduler.java
+++ src/main/java/com/ashleydev/jokeur_api/scheduler/RssScheduler.java
@@ -13,7 +13,7 @@ public class RssScheduler {
 
   private final NewsRssService newsRssService;
 
-  // "0 0/30 8-10 * * *" = 8:00, 8:30, 9:00, 9:30 and 10 o'clock every day.
+  // EXEMPLE : "0 0/30 8-10 * * *" = 8:00, 8:30, 9:00, 9:30 and 10 o'clock every day.
   @Scheduled(cron = "0 0/10 6-23 * * *")
   // SAUV : @Scheduled(cron = "0 0 */12 * * *")
   public void fetchRss() {
