f29f8225d5db5dd47c23ddd0c11d4d3a3ea7e98c
sarakadimi37 <sarakadimi37@gmail.com>
2026-03-09 22:28:51 +0100

    ajout annonatation Repository

--- src/main/java/com/ashleydev/jokeur_api/persistence/repositories/SymptomRepository.java
+++ src/main/java/com/ashleydev/jokeur_api/persistence/repositories/SymptomRepository.java
@@ -4,7 +4,9 @@ import com.ashleydev.jokeur_api.persistence.entities.SymptomEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SymptomRepository extends JpaRepository<SymptomEntity, Long> {
   boolean existsByNameIgnoreCase(String name);
 
