2d1ce6bc43100faa62173b392914817fbed4cf7a
sarakadimi37 <sarakadimi37@gmail.com>
2026-03-09 21:58:40 +0100

    correction of TU

--- src/test/java/com/ashleydev/jokeur_api/unit/SymptomRulesTest.java
+++ src/test/java/com/ashleydev/jokeur_api/unit/SymptomRulesTest.java
@@ -2,58 +2,62 @@ package com.ashleydev.jokeur_api.unit;
 
 import com.ashleydev.jokeur_api.domain.rules.SymptomRules;
 import com.ashleydev.jokeur_api.exceptions.symptom.BusinessException;
-import com.ashleydev.jokeur_api.persistence.entities.SymptomEntity;
+import com.ashleydev.jokeur_api.persistence.repositories.SymptomHealthRecordRepository;
 import com.ashleydev.jokeur_api.persistence.repositories.SymptomRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class SymptomRulesTest {
 
-    @Autowired
+    @InjectMocks
     private SymptomRules symptomRules;
 
-    @Autowired
+    @Mock
     private SymptomRepository symptomRepository;
+    @Mock
+    private SymptomHealthRecordRepository symptomHealthRecordRepository;
+
 
     @Test
     void checkExists_shouldThrowException_whenSymptomNotFound() {
-
-        Long id = 79L;
-
-        assertThrows(
+        Mockito.when(symptomRepository.existsById(Mockito.anyLong())).thenReturn(false);
+        Long id = 1L;
+        BusinessException ex = assertThrows(
                 BusinessException.class,
+                () -> symptomRules.checkExists(id)
+        );
+
+        assertTrue(ex.getMessage().contains("Symptom non trouvé"));
+        verify(symptomRepository).existsById(id);
+    }
+
+
+    @Test
+    void checkExists_shouldPass_whenSymptomExists() {
+
+        Long id = 5L;
+        when(symptomRepository.existsById(id)).thenReturn(true);
+
+        assertDoesNotThrow(
                 () -> symptomRules.checkExists(id)
         );
     }
 
     @Test
-    void checkExists_shouldPass_whenSymptomExists() {
-
-        SymptomEntity symptom = new SymptomEntity();
-        symptom.setName("Vomissements");
-
-        symptomRepository.save(symptom);
-
-        assertDoesNotThrow(
-                () -> symptomRules.checkExists(symptom.getId())
-        );
-    }
-
-    @Test
     void checkNameNotExists_shouldThrowException_whenNameAlreadyExist(){
-        SymptomEntity symptom = new SymptomEntity();
-        symptom.setName("Fièvre");
-        symptomRepository.save(symptom);
-
+        String fievre = "Fièvre";
+        when(symptomRepository.existsByNameIgnoreCase(fievre)).thenReturn(true);
         assertThrows(
-                BusinessException.class, ()->symptomRules.checkNameNotExists("Fièvre")
+                BusinessException.class, ()->symptomRules.checkNameNotExists(fievre)
         );
     }
 
