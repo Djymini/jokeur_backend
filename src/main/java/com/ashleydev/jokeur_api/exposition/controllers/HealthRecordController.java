389b74b36019bd095a3058d2a3c85519bfe071fb
yol97 <yol974@hotmail.com>
2026-03-11 16:16:48 +0100

    Profile update added

--- src/main/java/com/ashleydev/jokeur_api/exposition/controllers/HealthRecordController.java
+++ src/main/java/com/ashleydev/jokeur_api/exposition/controllers/HealthRecordController.java
@@ -16,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 @SuppressWarnings("unused")
 public class HealthRecordController {
 
-  private HealthRecordService healthRecordService; // titi
+  private HealthRecordService healthRecordService;
 
   @GetMapping
   public ResponseEntity<List<HealthRecordResponseDto>> getAllAnimals(@RequestParam(value = "userId", required = false) Long userId) {
