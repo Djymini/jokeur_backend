5586de15ef5aa191cd5ab3d4f0455537fe75a50b
Ashley <laboryashley@gmail.com>
2026-03-12 09:21:58 +0100

    allow public access to /uploads/** in security config

--- src/main/java/com/ashleydev/jokeur_api/security/SecurityConfig.java
+++ src/main/java/com/ashleydev/jokeur_api/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(auth ->
         auth
-          .requestMatchers("/auth/**", "/test/all", "/owners/**", "/auth/forgot-password", "/uploads")
+          .requestMatchers("/auth/**", "/test/all", "/owners/**", "/auth/forgot-password", "/uploads/**")
           .permitAll()
           .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
           .permitAll()
