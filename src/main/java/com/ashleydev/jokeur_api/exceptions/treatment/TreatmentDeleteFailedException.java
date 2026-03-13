bc257de27fd5c2e0df4b1f1d56a5214a1ab9b632
Mawele <mbizi.kevin@gmail.com>
2026-03-13 09:39:38 +0100

    fix one bug

--- src/main/java/com/ashleydev/jokeur_api/exceptions/treatment/TreatmentDeleteFailedException.java
+++ src/main/java/com/ashleydev/jokeur_api/exceptions/treatment/TreatmentDeleteFailedException.java
@@ -1,7 +1,6 @@
 package com.ashleydev.jokeur_api.exceptions.treatment;
 
 public class TreatmentDeleteFailedException extends RuntimeException {
-
   public TreatmentDeleteFailedException(String message) {
     super(message);
   }
--- src/main/java/com/ashleydev/jokeur_api/exceptions/treatment/TreatmentNotFoundException.java
+++ src/main/java/com/ashleydev/jokeur_api/exceptions/treatment/TreatmentNotFoundException.java
@@ -1,7 +1,6 @@
 package com.ashleydev.jokeur_api.exceptions.treatment;
 
 public class TreatmentNotFoundException extends RuntimeException {
-
   public TreatmentNotFoundException(Long id) {
     super("Treatment not found with id: " + id);
   }
