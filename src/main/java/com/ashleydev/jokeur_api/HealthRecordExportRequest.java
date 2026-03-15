58c7a8230dd2f37c77ab2d1f82482daad5f5cba5
Ashley <laboryashley@gmail.com>
2026-03-15 18:33:23 +0100

    prettier changes

--- src/main/java/com/ashleydev/jokeur_api/HealthRecordExportRequest.java
+++ src/main/java/com/ashleydev/jokeur_api/HealthRecordExportRequest.java
@@ -10,17 +10,17 @@ import lombok.Data;
 @Data
 public class HealthRecordExportRequest {
 
-    private LocalDate from;
+  private LocalDate from;
 
-    private LocalDate to;
+  private LocalDate to;
 
-    private List<@NotNull MeasureType> measureTypes;
+  private List<@NotNull MeasureType> measureTypes;
 
-    private boolean includeVaccines;
+  private boolean includeVaccines;
 
-    @AssertTrue(message = "The start date must be earlier than the end date")
-    public boolean isDateRangeValid() {
-        if (from == null || to == null) return true;
-        return !from.isAfter(to);
-    }
+  @AssertTrue(message = "The start date must be earlier than the end date")
+  public boolean isDateRangeValid() {
+    if (from == null || to == null) return true;
+    return !from.isAfter(to);
+  }
 }
